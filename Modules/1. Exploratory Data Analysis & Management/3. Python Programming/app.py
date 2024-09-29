6c2fd32450000c5006279492ca16940d086d5fad
Stephen-89 <160050389+Stephen-89@users.noreply.github.com>
2024-09-29 08:27:50 +0100

    cleanup

--- Modules/1. Exploratory Data Analysis & Management/3. Python Programming/app.py
+++ Modules/1. Exploratory Data Analysis & Management/3. Python Programming/app.py
@@ -4,34 +4,40 @@ import csv
 import statistics
 import matplotlib.pyplot as plt
 
-
 app = Flask(__name__)
 
-class HackerNews:
-    def __init__(self, id, title, url, num_points, num_comments, author, created_at):
-        self.id = id
-        self.title = title
-        self.url = url
-        self.num_points = num_points
-        self.num_comments = num_comments
-        self.author = author
-        self.created_at = created_at
+class CveVulnerabilities:
+    def __init__(self, cve_id, link, vendor, product, description, year, published_date, vector_string, base_score, impact_score, exploitability_score):
+        self.cve_id = cve_id
+        self.link = link
+        self.vendor = vendor
+        self.product = product
+        self.description = description
+        self.year = year
+        self.published_date = published_date
+        self.vector_string = vector_string
+        self.base_score = base_score
+        self.impact_score = impact_score
+        self.exploitability_score = exploitability_score
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'title': self.title,
-            'url': self.url,
-            'num_points': self.num_points,
-            'num_comments': self.num_comments,
-            'author': self.author,
-            'created_at': self.created_at
+            'cve_id': self.cve_id,
+            'link': self.link,
+            'vendor': self.vendor,
+            'product': self.product,
+            'description': self.description,
+            'year': self.year,
+            'published_date': self.published_date,
+            'vector_string': self.vector_string,
+            'base_score': self.base_score,
+            'impact_score': self.impact_score,
+            'exploitability_score': self.exploitability_score
         }
 
-def all_hacker_news(file_path):
+def all_vulnerabilities(file_path):
     
-    news_list = []
-    points_list = []
+    vulnerabilities = []
 
     with open(file_path, 'r', newline='', encoding='utf-8') as file:
         
@@ -39,102 +45,36 @@ def all_hacker_news(file_path):
         
         for row in reader:
             try:
-                # Extract fields
-                id = int(row.get('id', '0'))
-                title = row.get('title', '')
-                url = row.get('url', '')
-                num_points = int(row.get('num_points', '0'))
-                num_comments = int(row.get('num_comments', '0'))
-                author = row.get('author', '')
-                created_at = row.get('created_at', '')
+                
+                cve_id = int(row.get('cve_id', ''))
+                link = row.get('link', '')
+                vendor = row.get('vendor', '')
+                product = int(row.get('product', ''))
+                description = int(row.get('description', ''))
+                published_date = row.get('published_date', '')
+                vector_string = row.get('vector_string', '')
+                base_score = row.get('base_score', '')
+                impact_score = row.get('impact_score', '')
+                exploitability_score = row.get('exploitability_score', '')
 
-                # Create HackerNews object
-                post = HackerNews(id, title, url, num_points, num_comments, author, created_at)
-                news_list.append(post)
+                post = CveVulnerabilities(cve_id, link, vendor, product, description, published_date, vector_string. vector_string, base_score, impact_score, exploitability_score)
+                vulnerabilities.append(post)
 
-                # Add num_points to the list
-                points_list.append(num_points)
-            
             except ValueError:
                 continue
 
     return news_list, points_list
 
-file_path = Path('./data/hacker_news.csv')
-news_list, points_list = all_hacker_news(file_path)
-
-def get_points_stats():
-    
-    mean_value = statistics.mean(points_list)
-    median_value = statistics.median(points_list)
-    mode_value = statistics.mode(points_list)
-    std_value = statistics.stdev(points_list)
-    
-    return {
-            'values': points_list,
-            'mean': mean_value,
-            'median': median_value,
-            'mode': mode_value,
-            'std': std_value
-        }
-    
-def display_graph():
-    
-    # Get stats from the function
-    stats = get_points_stats()
-    
-    # Plotting with Matplotlib
-    # Create a figure with two subplots: one for stats and one for histogram
-    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-    
-    # Bar plot for mean, median, mode, std
-    axs[0].bar(['Mean', 'Median', 'Mode', 'Std'], [stats['mean'], stats['median'], stats['mode'], stats['std']], color=['blue', 'green', 'red', 'purple'])
-    axs[0].set_title('Statistical Summary')
-    axs[0].set_ylabel('Value')
-    
-    # Histogram for the points list
-    axs[1].hist(stats['values'], bins=5, color='skyblue', edgecolor='black')
-    axs[1].set_title('Distribution of Points')
-    axs[1].set_xlabel('Points')
-    axs[1].set_ylabel('Frequency')
-    
-    # Show the plot
-    plt.tight_layout()
-    plt.show()
-    
-def get_top_ten_most_comments(hacker_news):
-    sorted_comments = sorted(hacker_news, key=lambda x: x.num_comments, reverse=True)
-    top_ten = sorted_comments[:10]
-    return top_ten
-
-top_ten_comments = get_top_ten_most_comments(news_list)
-top_ten_dicts_comments = [post.to_dict() for post in top_ten_comments]
-
-def get_top_ten_most_points(hacker_news):
-    sorted_points = sorted(hacker_news, key=lambda x: x.num_points, reverse=True)
-    top_ten = sorted_points[:10]
-    return top_ten
-
-top_ten_points = get_top_ten_most_points(news_list)
-top_ten_dicts_points = [post.to_dict() for post in top_ten_points]
+file_path = Path('./data/vulnerability_data.csv')
+vulnerabilities = all_vulnerabilities(file_path)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/v1.0/points-stats', methods=['GET'])
-def points_stats():
-    result = get_points_stats()
-    return jsonify(result)
-
-@app.route('/api/v1.0/top-ten-comments', methods=['GET'])
-def news_list_comments():
-    return jsonify(top_ten_dicts_comments)
-
-@app.route('/api/v1.0/top-ten-points', methods=['GET'])
-def news_list_points():
-    display_graph()
-    return jsonify(top_ten_dicts_points)
+@app.route('/api/v1.0/vulnerabilities', methods=['GET'])
+def all_vulnerabilities():
+    return jsonify(vulnerabilities)
 
 if __name__ == '__main__':
     app.run(debug=False)
